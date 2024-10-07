4424e5af251d81955b10b8216fef11c02b7a99af
Yusuke Kajita <kaji@example.com>
2024-10-07 05:37:20 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
+  config.hosts << "af8f4fd1ae524334b8d2aabee390d11b.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
